90dedf2bdf9dc4fa8626f254eb231cc9078a26f4
thivernale <thivernale@gmail.com>
2025-04-14 20:20:02 +0300

    Add tailwindcss and icons

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
@@ -14,18 +15,26 @@ export default tseslint
       languageOptions: {
         ecmaVersion: 2020,
         globals: globals.browser,
+        parserOptions: {
+          project: ['./tsconfig.app.json', './tsconfig.node.json'],
+          tsconfigRootDir: import.meta.dirName,
+        },
       },
+      settings: { react: { version: '19.0' } },
       plugins: {
+        react,
         'react-hooks': reactHooks,
         'react-refresh': reactRefresh,
       },
       rules: {
         ...reactHooks.configs.recommended.rules,
+        ...react.configs.recommended.rules,
+        ...react.configs['jsx-runtime'].rules,
         'react-refresh/only-export-components': [
           'warn',
           { allowConstantExport: true },
         ],
       },
-    }
+    },
   )
   .concat(eslintPluginPrettier);
