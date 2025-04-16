c8a2064ead0ad33cc5f79f276f9b4649e222aa99
thivernale <thivernale@gmail.com>
2025-04-16 11:43:12 +0300

    Generate API for calling backend

---
+++ frontend/src/generated/models/index.ts
@@ -0,0 +1,9 @@
+// @ts-nocheck
+/* tslint:disable */
+/* eslint-disable */
+export * from './Conversation';
+export * from './ConversationRequest';
+export * from './CreateMatchRequest';
+export * from './Match';
+export * from './Message';
+export * from './Profile';
