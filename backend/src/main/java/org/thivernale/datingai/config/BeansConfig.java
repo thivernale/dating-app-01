0f031a79a521b6aa2454ec3d06a0768b4c5de0d6
thivernale <thivernale@gmail.com>
2025-04-17 12:05:00 +0300

    Add CORS filter

---
+++ backend/src/main/java/org/thivernale/datingai/config/BeansConfig.java
@@ -0,0 +1,29 @@
+package org.thivernale.datingai.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class BeansConfig {
+    @Value("${application.frontend.url:}")
+    private List<String> frontendUrl;
+
+    @Bean
+    CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(frontendUrl);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
